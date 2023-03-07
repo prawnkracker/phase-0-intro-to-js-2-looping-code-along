65e9826eae3bfb123d3e5cb9fa4ebdbc75b64b60
Dennis Demir <dennisjdemir@gmail.com>
2023-03-07 16:35:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//Code your solutions in this file
+
+
+function writeCards(names, value) {
+    const messageNames = [];
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    
+    } return messageNames;
+}
+
+writeCards(["Guadalupe","Ollie","Aki"], "surprise");
+
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
